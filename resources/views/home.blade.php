79cd11bf484d13c3f4f68a9f18abf5a6c65bdf25
soregashi-27 <tomo.soul.15@gmail.com>
2021-04-30 16:43:37 +0900

    投稿画像を追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,22 @@
                     <strong>212</strong>following
                 </div>
             </div>
+            <div class="pt-4 font-weight-bold">SNS-site.org</div>
+            <div>Hi there. I'm soregashi-27. I love coding, and create something a new one. Let's go enjoy making. My github below.</div>
+            <div><a href="https://github.com/soregashi-27">https://github.com/soregashi-27</a></div>
+        </div>
+    </div>
 
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="images/laravel_midnightHacking.jpg" class="w-100" alt="postPictureFirst">
+
+        </div>
+        <div class="col-4">
+            <img src="images/laravel_midnightProgramming.jpg" class="w-100" alt="postPictureSecond">
+        </div>
+        <div class="col-4">
+            <img src="images/laravel_reviewBefore.jpg" class="w-100" alt="postPictureThird">
         </div>
 
     </div>
