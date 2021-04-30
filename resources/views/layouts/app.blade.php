3ae3d3c4e93364c45f152defb9c411cf70a63430
soregashi-27 <tomo.soul.15@gmail.com>
2021-04-30 12:23:55 +0900

    fontの変更、ヘッダー作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,9 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{-- <img src="" style="height: 25px; border-right: 1px; solid: #333;" alt="Logo"> --}}
+                    {{-- TODO: add Logo --}}
+                    <div class="pl-3">SNS-site</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
